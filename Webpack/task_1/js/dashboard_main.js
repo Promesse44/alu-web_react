21e68f2bdbc47f1797d7e5cb98d70d59a44eac20
Promesse <p.irakoze2@alustudent.com>
2026-09-09 14:01:54 +0200

    task 1

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
 import $ from 'jquery';
-import _ from 'lodash';
+import debounce from 'lodash/debounce';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="get-started">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+  let count = 0;
 
-function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
+  const updateCounter = () => {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+  };
 
-$('button').on('click', _.debounce(updateCounter, 500));
+  $('#get-started').on('click', debounce(updateCounter, 300));
+});
